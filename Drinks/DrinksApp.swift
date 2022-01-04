7e4de326c5eae08e7caba87396a6620c37c58eca
KendiJ <kendyjaky@gmail.com>
2022-01-04 23:04:08 +0300

    Initial Commit

---
+++ Drinks/DrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksApp.swift
+//  Drinks
+//
+//  Created by Kendi J on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
